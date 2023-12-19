d2ec9b77c17fb77f88f2abf0f30f416a61378b63
moreiraa97 <lukaas.livebr@gmail.com>
2023-12-19 16:41:07 -0300

    first commit with simple tasks just to show the encapsulation process

---
+++ message_aplication/src/message_aplication/UserPc.java
@@ -0,0 +1,13 @@
+package message_aplication;
+
+public class UserPc {
+	
+	public static void main(String[] args) {
+	
+	MsnMessenger msn = new MsnMessenger();
+	
+	msn.enviarMensagem();
+	msn.receberMensagem(); 
+
+	}
+}
---
+++ message_aplication/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module message_aplication {
+}
